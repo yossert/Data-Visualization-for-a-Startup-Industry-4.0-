86500db6e1a1f86ede62224e780acc7195930e46
yossert <92958823+yossert@users.noreply.github.com>
2024-09-20 14:20:45 +0100

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style1.css">
+        <title>4IOT</title>
+    </head>
+    <body>
+        <div class="main">
+            <div class="navbar">
+            </div>
+            </div class="vid">
+                        <video autoplay muted loop id="myVideo">
+                            <source src="vid1.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+            </div>
+        </div>
+    </body>
